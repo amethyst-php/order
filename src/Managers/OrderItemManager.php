a724fe30436cd7f27ddff234e0af6886d1f41f89
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/OrderItemManager.php
+++ src/Managers/OrderItemManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\OrderItem newEntity()
+ * @method \Amethyst\Schemas\OrderItemSchema getSchema()
+ * @method \Amethyst\Repositories\OrderItemRepository getRepository()
+ * @method \Amethyst\Serializers\OrderItemSerializer getSerializer()
+ * @method \Amethyst\Validators\OrderItemValidator getValidator()
+ * @method \Amethyst\Authorizers\OrderItemAuthorizer getAuthorizer()
+ */
 class OrderItemManager extends Manager
 {
     use ConfigurableManager;
--- src/Managers/OrderManager.php
+++ src/Managers/OrderManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Order newEntity()
+ * @method \Amethyst\Schemas\OrderSchema getSchema()
+ * @method \Amethyst\Repositories\OrderRepository getRepository()
+ * @method \Amethyst\Serializers\OrderSerializer getSerializer()
+ * @method \Amethyst\Validators\OrderValidator getValidator()
+ * @method \Amethyst\Authorizers\OrderAuthorizer getAuthorizer()
+ */
 class OrderManager extends Manager
 {
     use ConfigurableManager;
